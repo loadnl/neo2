22f76e7fa3024e625a323da28624ab08f4317a8b
loadnl <loadling.hh@gmail.com>
2024-09-05 21:46:03 +0800

    Update and rename surge.js to test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 hostname = surge-activation.com
-^https?:\/\/surge-activation\.com\/ios/v\d/refresh url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/caiyun_json.js
+^https?:\/\/surge-activation\.com\/ios/v\d/refresh url script-response-body https://raw.githubusercontent.com/loadnl/neo2/main/test.js
 
 let responseBody={};$request.url.includes("/ios/v3/refresh") ? ((responseBody = JSON.parse($response.body)).deviceID  = "63989092405B", responseBody.result.deviceID ="63989092405B” )$done({}), $done({ body: JSON.stringify(responseBody) });

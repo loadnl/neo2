ec652b703758856931fcca3b3684be24f6f40a78
loadnl <loadling.hh@gmail.com>
2024-09-05 22:05:14 +0800

    Update test.js

--- test.js
+++ test.js
@@ -4,4 +4,8 @@
 [mitm]
 hostname = www.surge-activation.com
 
-$request.url.includes("/ios/v3/refresh") ? ((responseBody = JSON.parse($response.body)).deviceID  = "63989092405B", responseBody.result.deviceID ="63989092405B” )$done({}), $done({ body: JSON.stringify(responseBody) });
+let responseBody = {};
+$request.url.includes("/ios/v3/refresh") ? ((responseBody = JSON.parse($response.body)).deviceID = "63989092405B") : $done({}),
+$done({
+    body: JSON.stringify(responseBody)
+});

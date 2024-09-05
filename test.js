5b7348538a1619d68f657c8c00dc8caeb7ecb156
loadnl <loadling.hh@gmail.com>
2024-09-05 22:01:49 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 [rewrite_local]
-^https?:\/\/surge-activation\.com\/ios/v\d/refresh$ url script-response-body https://raw.githubusercontent.com/loadnl/neo2/main/test.js
+^https?:\/\/www\.surge-activation\.com\/ios/v\d/refresh$ url script-response-body https://raw.githubusercontent.com/loadnl/neo2/main/test.js
 
 [mitm]
-hostname = surge-activation.com
+hostname = www.surge-activation.com
 
 $request.url.includes("/ios/v3/refresh") ? ((responseBody = JSON.parse($response.body)).deviceID  = "63989092405B", responseBody.result.deviceID ="63989092405B” )$done({}), $done({ body: JSON.stringify(responseBody) });

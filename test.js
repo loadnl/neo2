8496adbcf141655002e52c6c6f3377ec0b6bf0d5
loadnl <loadling.hh@gmail.com>
2024-09-05 21:46:47 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,2 @@
-hostname = surge-activation.com
-^https?:\/\/surge-activation\.com\/ios/v\d/refresh url script-response-body https://raw.githubusercontent.com/loadnl/neo2/main/test.js
 
 let responseBody={};$request.url.includes("/ios/v3/refresh") ? ((responseBody = JSON.parse($response.body)).deviceID  = "63989092405B", responseBody.result.deviceID ="63989092405B” )$done({}), $done({ body: JSON.stringify(responseBody) });
